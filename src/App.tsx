b7c3eeea49234023162cf09c501260f1afe79ebd
marioestima <marioestimahackz@gmail.com>
2025-11-18 19:59:20 +0100

    fix some services and deploy without bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
-import { AuthProvider } from "./hooks/useAuth";
+import { AuthProvider } from "./context/AuthContext";
 
 const App: React.FC = () => {
   return (
