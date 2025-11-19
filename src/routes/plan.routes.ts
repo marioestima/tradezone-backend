4871ea791f59e2fff95afd393af9d8badf68ef8f
marioestima <marioestimahackz@gmail.com>
2025-11-19 02:33:20 +0100

    auth mid

--- src/routes/plan.routes.ts
+++ src/routes/plan.routes.ts
@@ -1,19 +1,25 @@
 import { Router } from "express";
 import { PlanController } from "../controllers/plan-controller";
- 
+import { authMiddleware } from "../middlewares/auth-middleware";
 
 const router = Router();
 const planController = new PlanController();
 
-// Criar plano
-router.post("/", planController.create.bind(planController));
+// Criar plano → só usuário autenticado
+router.post("/", authMiddleware, planController.create.bind(planController));
 
-// Listar todos os planos
-router.get("/", planController.getAll.bind(planController));
+// Listar todos os planos → público (sem middleware) ou autenticado se preferir
+router.get(
+  "/",
+  authMiddleware, // opcional
+  planController.getAll.bind(planController)
+);
 
-// Fechar plano
+// Fechar plano → apenas admin
 router.post(
   "/close/:id",
+  authMiddleware,
+
   planController.close.bind(planController)
 );
 
